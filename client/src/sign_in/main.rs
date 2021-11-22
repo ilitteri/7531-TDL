2a7100af01f5fd47c7e187dce5c30a1755a5a80f
Ivan <ilitteri@fi.uba.ar>
2021-11-22 16:18:41 -0300

    Add: sign_in main & client account file Co-authored-by: Agustina Bocaccio <agusbocaccio@users.noreply.github.com>

--- client/src/sign_in/main.rs
+++ client/src/sign_in/main.rs
@@ -1,32 +1,60 @@
-use std::{ error, result };
+use std::io;
 
-type TResult<T> = result::Result<T, TError>;
-type TError = Box<dyn error::Error>;
+mod client_account;
+use crate::client_account::ClientAccount;
 
-#[cfg(test)]
-mod test {
-    use super::*;
 
-    #[test]
-    fn test01_() {
-        let name = "Pedro";
-        let lastname = "Messi";
-        let email = "pmessi@fi.uba.ar";
-        let password = "messid10s";
-        let birth_date = "13/04/2000";
-        let dni = "436463234";
+fn main() {
+    let mut name = String::new();
+    let mut lastname = String::new();
+    let mut email = String::new();
+    let mut password = String::new();
+    let mut birth_date = String::new();
+    let mut dni = String::new();
 
-        let account = ClientAccount::new(
-            name: name,
-            lastname: lastname,
-            email: email,
-            password: password,
-            birth_date: birthdate,
-            dni: dni
-        );
+    println!("Ingresá tu nombre:");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
 
-        assert(account);
-    }
+    println!("Ingresa tu apellido:");
+    io::stdin()
+        .read_line(&mut lastname)
+        .expect("Failed to read line");
+
+    println!("Ingresa tu email:");
+    io::stdin()
+        .read_line(&mut email)
+        .expect("Failed to read line");
+
+    println!("Ingresa tu contraseña:");
+    io::stdin()
+        .read_line(&mut password)
+        .expect("Failed to read line");
+
+    println!("Ingresa tu Fecha de Nacimiento:");
+    io::stdin()
+        .read_line(&mut birth_date)
+        .expect("Failed to read line");
+    
+    println!("Ingresa:");
+    io::stdin()
+        .read_line(&mut dni)
+        .expect("Failed to read line");
+    
+    let client_account = ClientAccount::new(
+        name.trim(),
+        lastname.trim(),
+        email.trim(),
+        password.trim(),
+        birth_date.trim(),
+        dni.trim(),
+    );
+
+    println!("Nombre: {}", client_account.name);
+    println!("Apellido: {}", client_account.lastname);
+    println!("Email: {}", client_account.email);
+    println!("Contraseña: {}", client_account.name);
+    println!("Fecha de nacimiento: {}", client_account.name);
+    println!("DNI: {}", client_account.name);
 }
-
-// impl ClientAccount
